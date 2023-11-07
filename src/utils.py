56dc98a1554142a6c5e4f3ee6d4c11a0820e246e
Ekaterina Salnikova <sekateri@mail.ru>
2023-11-08 00:59:31 +0300

    add sorted_by_date, add sort_operations

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import json
 import os
+from datetime import datetime
 
 def load_operations(path):
     """
@@ -21,6 +22,17 @@ def choose_operations(all_operations):
         if operation.get('state') == 'EXECUTED':
             executed_operations.append(operation)
     return executed_operations
+
+def sorted_by_date(op):
+    return datetime.strptime(op['date'],  '%Y-%m-%dT%H:%M:%S.%f')
+
+def sort_operations(executed_operations):
+    """
+    сортирует операции по дате и выбирает 5 последних
+    :return:
+    """
+    sorted_operations = sorted(executed_operations, key=lambda op: sorted_by_date(op), reverse=True)
+    return sorted_operations[:5]
 
 
 
