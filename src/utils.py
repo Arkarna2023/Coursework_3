0c8ad3a4b38e1bff8cff77de8bd80a8327b8994e
Ekaterina Salnikova <sekateri@mail.ru>
2023-11-07 19:53:42 +0300

    add load_operations, choose_operations

--- src/utils.py
+++ src/utils.py
@@ -8,4 +8,24 @@ def load_operations(path):
     :return:
     """
     with open(path, "r", encoding="utf-8") as file:
-        return json.load(file)
+        all_operations = json.load(file)
+        return all_operations
+
+def choose_operations(all_operations):
+    """
+    выбирает операции (EXECUTED)
+    :return:
+    """
+    executed_operations = []
+    for operation in all_operations:
+        if operation.get('state') == 'EXECUTED':
+            executed_operations.append(operation)
+    return executed_operations
+
+
+
+
+
+
+
+
